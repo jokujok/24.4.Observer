98df6840dc032aee5e58e51bccbc98b0f4895acb
Artur Cieslinski <jokujok1492@gmail.com>
2022-04-10 22:30:39 +0200

    7.4.CountingPeopleOnTheWorld

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Continent.java
@@ -0,0 +1,18 @@
+package com.kodilla.stream.world;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Continent {
+
+    private final List<Country> countries = new ArrayList<>();
+
+    public void addCountry() {
+        countries.add();
+        countries.add();
+        countries.add();
+        countries.add();
+        countries.add();
+    }
+
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -0,0 +1,22 @@
+package com.kodilla.stream.world;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class World {
+
+    private final List<Continent> continents = new ArrayList<>();
+
+    public void addContinent() {
+        continents.add();
+        continents.add();
+        continents.add();
+        continents.add();
+        continents.add();
+    }
+    public BigDecimal getPeopleQuantity() {
+        BigDecimal peopleQuantity = new BigDecimal("8000000000");
+                return peopleQuantity;
+    }
+}
